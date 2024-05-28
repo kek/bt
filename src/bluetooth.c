d1e1d398018e9381343652d92c13e50bf8a3ffe9
Karl Eklund <localpart@gmail.com>
2024-05-28 09:33:39 +0200

    Get Bluetooth adapters

--- src/bluetooth.c
+++ src/bluetooth.c
@@ -1,10 +1,20 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-extern void SendGoMessage(char* message);
+#include "simpleble_c/adapter.h"
+#include "simpleble_c/simpleble.h"
+#include "simpleble_c/types.h"
 
-void bluetooth_scan()
-{
-    printf("Hello\n");
-    SendGoMessage("Hello world");
+extern void SendGoMessage(char *message);
+
+void bluetooth_scan() {
+  printf("Hello\n");
+
+  simpleble_err_t err_code = SIMPLEBLE_SUCCESS;
+
+  size_t adapter_count = simpleble_adapter_get_count();
+  printf("Adapter count: %d\n", (int)adapter_count);
+  simpleble_adapter_get_handle(0);
+  SendGoMessage("Hello world");
 }
